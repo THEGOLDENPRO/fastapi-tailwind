d61978eb82556ab6a17705efc85241e7e97dc132
Goldy <66202304+THEGOLDENPRO@users.noreply.github.com>
2024-08-14 01:46:38 +0100

    docs: add another comment to example code

--- examples/hello-world-app/main.py
+++ examples/hello-world-app/main.py
@@ -9,13 +9,16 @@ static_files = StaticFiles(directory = "static")
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    # YAY, our tailwind get's compiled here! 😄
     process = tailwind.compile(static_files.directory + "/output.css")
 
-    yield
+    yield # The code after this is called on shutdown.
 
-    process.terminate()
+    process.terminate() # We must terminate the compiler on shutdown to
+    # prevent multiple compilers running in development mode or when watch is enabled.
 
 app = FastAPI(
+    # See the fastapi documentation for an explanation on lifespans: https://fastapi.tiangolo.com/advanced/events/
     lifespan = lifespan
 )
 
@@ -23,4 +26,5 @@ app = FastAPI(
 def index():
     return FileResponse("./index.html")
 
+# We need somewhere to drop the compiled stylesheet so our html file can link it.
 app.mount("/static", static_files, name = "static")
